ed2dbbd25c73f6fbcd8e5718b200cfde8ac13c6b
AdityasinhSolanki <adityasolanki282@gmail.com>
2025-06-17 13:49:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+Weather API
+You want to develop a weather application that provides users with real-time weather data for their current location. You decide to use a free weather API to retrieve the weather data. Write a program that demonstrates how to access the free weather API, pass headers, and handle the HTTP response to display the weather data.
+
+Demo
+
+Instructions
+Create a button with the text as Get Current Weather
+On click of the button, get the weather data for London
+Update the div with the id as weatherData in the following format: Current weather in London: Clouds
+API to use
+https://openweathermap.org/api
